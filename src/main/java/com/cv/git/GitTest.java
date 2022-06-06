9458ce458f8691fa8566f83d3fced3a7e18c435d
cv <cv@qq.com>
2022-06-06 18:17:54 +0800

    hot-fix second commit

--- src/main/java/com/cv/git/GitTest.java
+++ src/main/java/com/cv/git/GitTest.java
@@ -3,5 +3,9 @@ package com.cv.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("hello git1");
+        System.out.println("hello git1");
+        System.out.println("hello git1");
+        System.out.println("hello git1");
     }
 }
